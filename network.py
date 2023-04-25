ee3bf0d79e1962dba0e7c872028ea3e39fd4429e
Xuefeng Hu <xh244@cornell.edu>
2023-04-25 16:59:11 -0400

    add average watch percentage

--- network.py
+++ network.py
@@ -1,5 +1,6 @@
 import csv
 import json
+import isodate
 
 network = csv.DictReader(open("./data/persistent_network.csv"))
 videos_info = []
@@ -39,6 +40,7 @@ with open("./data/vevo_en_embeds_60k.txt", "r") as mapping_info:
     vid_to_eid[vid] = eid
     
   
+count = 0
 for edge in network:
   source_eid = int(edge["Source"])
   target_eid = int(edge["Target"])
@@ -51,10 +53,16 @@ for video_info in videos_info:
   vid = video_info["id"]
   output[vid]["average_daily_view"] = video_info["insights"]["totalView"] / len(video_info["insights"]["dailyView"])
     
-  if video_info["insights"].get("totalShare") == None or video_info["insights"].get("dailyShare") == None:
+  if video_info["insights"].get("totalShare") == None or video_info["insights"].get("dailyShare") == None or video_info["insights"].get("avgWatch") == None:
     del output[vid]
+    count += 1
   else:
     output[vid]["average_daily_share"] = video_info["insights"]["totalShare"] / len(video_info["insights"]["dailyShare"])
-
+    if ((isodate.parse_duration(video_info["contentDetails"]["duration"]).total_seconds() / 60) != 0):
+      output[vid]["average_watch_percentage"] = video_info["insights"]["avgWatch"] / (isodate.parse_duration(video_info["contentDetails"]["duration"]).total_seconds() / 60)
+    else:
+      print(isodate.parse_duration(video_info["contentDetails"]["duration"]).total_seconds())
+    
+print(count)
 with open("output.json", "w") as output_file:
   output_file.write(json.dumps(output, indent=4))
