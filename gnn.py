8523fbdc584c7eb8b11405553f2835d7514c5bea
Mavis Lee <mengyun.lee@gmail.com>
2023-04-26 21:21:25 -0400

    update gpu

--- gnn.py
+++ gnn.py
@@ -14,18 +14,24 @@ class GCN(torch.nn.Module):
         x = self.conv2(x, edge_index)
         return F.log_softmax(x, dim=1)
 
-edge_index = torch.tensor([[0, 1, 2, 3, 4], [1, 2, 3, 4, 0]], dtype=torch.long)
+if torch.cuda:
+    device = 'cuda'
+else:
+    device = 'cpu'
+print(device)
+
+edge_index = torch.tensor([[0, 1, 2, 3, 4], [1, 2, 3, 4, 0]], dtype=torch.long).to(device)
 x = torch.tensor([[1.0, 2.0],
                   [2.0, 3.0],
                   [3.0, 4.0],
                   [4.0, 5.0],
                   [5.0, 6.0],
                   [0.0, 0.0],
-                  [0.0, 0.0]], dtype=torch.float)
+                  [0.0, 0.0]], dtype=torch.float).to(device)
 
-y = torch.tensor([0, 1, 1, 0, 1, 0, 0], dtype=torch.long)
+y = torch.tensor([0, 1, 1, 0, 1, 0, 0], dtype=torch.long).to(device)
 
-model = GCN(in_channels=2, hidden_channels=16, out_channels=5)
+model = GCN(in_channels=2, hidden_channels=16, out_channels=5).to(device)
 optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
 
 model.train()
@@ -40,10 +46,13 @@ for epoch in range(50):
 model.eval()
 correct = 0
 total = 0
-x_test = torch.tensor([[0.5, 0.6], [1.0, 1.1], [2.0, 2.1], [0.0, 0.0], [0.0, 0.0]], dtype=torch.float)
-y_test = torch.tensor([1, 1, 0, 1, 0])
+x_test = torch.tensor([[0.5, 0.6], [1.0, 1.1], [2.0, 2.1], [0.0, 0.0], [0.0, 0.0]], dtype=torch.float).to(device)
+y_test = torch.tensor([1, 1, 0, 1, 0]).to(device)
 out_test = model(x_test, edge_index)
 pred_test = out_test[:5].argmax(dim=1)
+if device == 'cuda':
+    pred_test = pred_test.cpu()
+    y_test = y_test.cpu()
 correct += pred_test.eq(y_test).sum().item()
 total += y.size(0)
 print('Predictions: {}'.format(pred_test.detach().numpy()))
